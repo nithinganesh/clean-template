0cb92529e70cead1b3463880e37d6e2321acbc12
nithinganesh <ganesh.nithin@in.bosch.com>
2020-08-03 17:08:09 +0530

    Added function details

--- vehicle_collector.h
+++ vehicle_collector.h
@@ -22,7 +22,7 @@ struct inventory
     float battery_temperature;
 };
 
-static inventory inventory_details[] =
+static inventory inventory_data[] =
                     {{1,std::numeric_limits<double>::quiet_NaN(),std::numeric_limits<double>::quiet_NaN(),std::numeric_limits<double>::quiet_NaN()},
                      {2,std::numeric_limits<double>::quiet_NaN(),std::numeric_limits<double>::quiet_NaN(),std::numeric_limits<double>::quiet_NaN()},
                      {3,std::numeric_limits<double>::quiet_NaN(),std::numeric_limits<double>::quiet_NaN(),std::numeric_limits<double>::quiet_NaN()}   };
@@ -38,8 +38,36 @@ inventory *fetch_inventory_reference(int vehicle_id)
     return nullptr;
 }
 
+void update_battery_temperature( inventory *details, float new_value)
+{
+    details->battery_temperature = new_value;
+}
+
+void update_motor_temperature( inventory *details, float new_value)
+{
+    details->motor_temperature = new_value;
+}
+
+void update_battery_pc( inventory *details, float new_value)
+{
+    details->battery_pc = new_value;
+}
+
 inventory telematics_to_inventory(const telematics& received_telematics)
 {
-    //auto inventory_details = fetch_inventory_reference(received_telematics.vehicle_id);   
-    return inventory_details[0];
+    auto fetched_inventory_details = fetch_inventory_reference(received_telematics.vehicle_id);
+    switch(received_telematics.type)
+    {
+        case measurement::battery_pc:
+            update_battery_pc(fetched_inventory_details,received_telematics.value);
+            break;
+        case measurement::battery_temperature:
+            update_battery_temperature(fetched_inventory_details,received_telematics.value);
+            break;
+        case measurement::motor_temperature:
+            update_motor_temperature(fetched_inventory_details,received_telematics.value);
+            break;
+    }
+
+    return *fetched_inventory_details;
 }
